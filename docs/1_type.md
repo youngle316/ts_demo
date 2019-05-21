85e1a2936deff0e17c1424bb6bafe404abb482a5
yanglele <youngle316@gmail.com>
2019-05-21 22:20:56 +0800

    Update 基础类型

--- docs/1_type.md
+++ docs/1_type.md
@@ -90,3 +90,193 @@ x[6] = true // Error, 布尔不是(string | number)类型
 
 **注意**：自从 TyeScript 3.1 版本之后，访问越界元素会报错，不应该再使用该特性。
 
+## 枚举
+
+`enum` 类型是对 JavaScript 标准数据类型的一个补充。 像 C# 等其它语言一样，使用枚举类型可以为一组数值赋予友好的名字。
+
+```typescript
+enum Color {
+  blue,
+  red,
+  yellow
+}
+
+let color: Color = Color.blue;
+```
+
+默认情况下，从 `0` 开始为元素编号。 也可以手动的指定成员的数值。 例如，下面从2开始进行编号，依次是3，4：
+
+```typescript
+enum Fruit {
+  orange = 2,
+  peach,
+  banana
+}
+
+let fruit: Fruit = Fruit.banana;
+```
+
+或者，也可以进行全部手动编号，如下面全部进行编号，且不按照顺序:
+
+```typescript
+enum People {
+  child = 1,
+  adult = 2,
+  old = 4
+}
+
+let people: People = People.adult;
+```
+
+枚举类型提供的一个便利是你可以由枚举的值得到它的名字。 例如，我们知道数值为 2，但是不确定它映射到 Color 里的哪个名字，我们可以查找相应的名字：
+
+```typescript
+enum Color2 {
+  green,
+  pink,
+  black
+}
+
+let colorName: string = Color2[2];
+console.log(colorName); // black
+```
+
+下面是ts枚举编译成js的样子
+
+```javascript
+var Color2;
+(function (Color2) {
+    Color2[Color2["green"] = 0] = "green";
+    Color2[Color2["pink"] = 1] = "pink";
+    Color2[Color2["black"] = 2] = "black";
+})(Color2 || (Color2 = {}));
+var colorName = Color2[2];
+console.log(colorName);
+```
+
+## any
+
+有时候，我们会想要为那些在编程阶段还不清楚类型的变量指定一个类型。 这些值可能来自于动态的内容，比如来自用户输入或第三方代码库。 这种情况下，我们不希望类型检查器对这些值进行检查而是直接让它们通过编译阶段的检查。 那么我们可以使用 `any` 类型来标记这些变量：
+
+```typescript
+// 直接通过编译阶段的类型检查
+let notSure: any = 1;
+notSure = "2";
+notSure = [1, 2, 3];
+```
+
+在对现有代码进行改写的时候，`any` 类型是十分有用的，它允许你在编译时可选择地包含或移除类型检查。并且当你只知道一部分数据的类型时，`any` 类型也是有用的。 比如，你有一个数组，它包含了不同的类型的数据：
+
+```typescript
+// 不确定数组元素的类型
+let list3: any[] = [1, true, 'free'];
+list3[1] = 100;
+```
+
+## void
+
+某种程度上来说，`void` 类型像是与 `any` 类型相反，它表示没有任何类型。 当一个函数没有返回值时，你通常会见到其返回值类型是 `void`：
+
+```typescript
+function warnUser(): void {
+  console.log('This is my warning message')
+}
+```
+
+声明一个 `void` 类型的变量没有什么大用，因为你只能为它赋予 `undefined` 和 `null`：
+
+```typescript
+let unuseable: void = undefined
+```
+
+## null和undefined
+
+TypeScript 里，`undefined` 和 `null` 两者各自有自己的类型分别叫做 `undefined` 和 `null`。 和 `void` 相似，它们的本身的类型用处不是很大：
+
+```typescript
+let u: undefined = undefined
+let n: null = null
+```
+
+默认情况下 `null` 和 `undefined` 是所有类型的子类型。 就是说你可以把 `null` 和 `undefined` 赋值给 `number` 类型的变量。
+
+然而，当你指定了 `--strictNullChecks` 标记，`null` 和 `undefined` 只能赋值给 `void` 和它们各自，这能避免 很多常见的问题。 也许在某处你想传入一个 `string` 或 `null` 或 `undefined`，你可以使用联合类型 `string | null | undefined`。
+
+```typescript
+// 指定了 --strictNullChecks标记，null和undefined只能赋给它们各自
+let num: number = 3;
+num = null; // 报错  Type 'null' is not assignable to type 'number'.
+
+// 使用联合类型可以解决这个问题
+let num1: number | null = 4;
+num1 = null;
+```
+
+## never
+
+`never` 类型表示的是那些永不存在的值的类型。 例如， `never` 类型是那些总是会抛出异常或根本就不会有返回值的函数表达式或箭头函数表达式的返回值类型； 变量也可能是 `never` 类型，当它们被永不为真的类型保护所约束时。
+
+`never` 类型是任何类型的子类型，也可以赋值给任何类型；然而，没有类型是 `never` 的子类型或可以赋值给`never` 类型（除了 `never` 本身之外）。 即使 `any` 也不可以赋值给 `never`。
+
+下面是一些返回 `never` 类型的函数：
+
+```typescript
+// 返回never的函数必须存在无法达到的终点
+function error(message: string): never {
+  throw new Error(message)
+}
+
+// 推断的返回值类型为never
+function fail() {
+  return error("Something failed")
+}
+
+// 返回never的函数必须存在无法达到的终点
+function infiniteLoop(): never {
+  while (true) {
+  }
+}
+```
+
+## object
+
+`object` 表示非原始类型，也就是除 `number`，`string`，`boolean`，`symbol`，`null`或`undefined` 之外的类型。
+
+使用 `object` 类型，就可以更好的表示像 `Object.create` 这样的 `API`。例如：
+
+```typescript
+declare function create(o: object | null): void
+
+create({ prop: 0 }) // OK
+create(null) // OK
+
+create(42) // Error
+create('string') // Error
+create(false) // Error
+create(undefined) // Error
+```
+
+## 类型断言
+
+有时候你会遇到这样的情况，你会比 TypeScript 更了解某个值的详细信息。 通常这会发生在你清楚地知道一个实体具有比它现有类型更确切的类型。
+
+通过类型断言这种方式可以告诉编译器，“相信我，我知道自己在干什么”。 类型断言好比其它语言里的类型转换，但是不进行特殊的数据检查和解构。 它没有运行时的影响，只是在编译阶段起作用。 TypeScript 会假设你，程序员，已经进行了必须的检查。
+
+类型断言有两种形式。 其一是“尖括号”语法：
+
+```typescript
+// “尖括号”语法
+let someValue: any = 'this is a string';
+let strLength: number = (<string>someValue).length;
+
+```
+
+另一个为 `as` 语法：
+
+```typescript
+// as语法
+let someValue1: any = 'this is a string';
+let strLength2: number = (someValue as string).length;
+```
+
+两种形式是等价的。 至于使用哪个大多数情况下是凭个人喜好；然而，当你在 TypeScript 里使用 JSX 时，只有 `as` 语法断言是被允许的。
--- examples/1_type.js
+++ examples/1_type.js
@@ -1,180 +1,136 @@
 /**
  * 布尔值
  */
-
-let isDone: boolean = true;
-
+var isDone = true;
 /**
  * 数字
  */
-
 // 十进制
-let DecimalLiteral: number = 16;
+var DecimalLiteral = 16;
 // 十六进制
-let hexadecimalLiteral: number = 0x14;
+var hexadecimalLiteral = 0x14;
 // 二进制
-let binaryLiteral: number = 0b10100;
+var binaryLiteral = 20;
 // 八进制
-let octalLiteral: number = 0o24;
-
+var octalLiteral = 20;
 /**
  * 字符串
  */
-
-let firstName: string = 'yang';
-let lastName: string = 'le';
-let sentence: string = `Hello, my name is ${firstName} ${lastName}`;
-
+var firstName = 'yang';
+var lastName = 'le';
+var sentence = "Hello, my name is " + firstName + " " + lastName;
 /**
  * 数组
  */
-
 // 元素类型后面接上 []，表示由此类型元素组成的一个数组
-let list: number[] = [1, 2, 3];
+var list = [1, 2, 3];
 // 使用数组泛型,Array<元素类型>
-let list2: Array<number> = [1, 2, 3];
-
+var list2 = [1, 2, 3];
 /**
  * 元祖tuple
  */
-
 // 定义一个元祖类型
-let x: [string, number];
+var x;
 // 初始化
 x = ['1', 2];
-
 // 当访问一个已知索引的元素，会得到正确的类型
 x[0].substr(1);
 // x[1].substr(1);    错误，数组没有substr方法
-
 // 当访问一个越界的元素，会使用联合类型替代
-
 /**
  * 枚举
  */
-
 // 默认从0开始编号
-enum Color {
-  blue,
-  red,
-  yellow
-}
-
-let color: Color = Color.blue;
-
+var Color;
+(function (Color) {
+    Color[Color["blue"] = 0] = "blue";
+    Color[Color["red"] = 1] = "red";
+    Color[Color["yellow"] = 2] = "yellow";
+})(Color || (Color = {}));
+var color = Color.blue;
 // 也可以手动编号，如下面从2开始进行编号，依次是3，4
-
-enum Fruit {
-  orange = 2,
-  peach,
-  banana
-}
-
-let fruit: Fruit = Fruit.banana;
-
+var Fruit;
+(function (Fruit) {
+    Fruit[Fruit["orange"] = 2] = "orange";
+    Fruit[Fruit["peach"] = 3] = "peach";
+    Fruit[Fruit["banana"] = 4] = "banana";
+})(Fruit || (Fruit = {}));
+var fruit = Fruit.banana;
 // 也可以进行全部手动编号，如下面全部进行编号，且不按照顺序
-
-enum People {
-  child = 1,
-  adult = 2,
-  old = 4
-}
-
-let people: People = People.adult;
-
+var People;
+(function (People) {
+    People[People["child"] = 1] = "child";
+    People[People["adult"] = 2] = "adult";
+    People[People["old"] = 4] = "old";
+})(People || (People = {}));
+var people = People.adult;
 // 可以有枚举的数值找到它的名字
-
-enum Color2 {
-  green,
-  pink,
-  black
-}
-
-let colorName: string = Color2[2];
+var Color2;
+(function (Color2) {
+    Color2[Color2["green"] = 0] = "green";
+    Color2[Color2["pink"] = 1] = "pink";
+    Color2[Color2["black"] = 2] = "black";
+})(Color2 || (Color2 = {}));
+var colorName = Color2[2];
 console.log(colorName);
-
 /**
  * any
  */
-
 // 直接通过编译阶段的类型检查
-let notSure: any = 1;
+var notSure = 1;
 notSure = "2";
 notSure = [1, 2, 3];
-
 // 不确定数组元素的类型
-let list3: any[] = [1, true, 'free'];
+var list3 = [1, true, 'free'];
 list3[1] = 100;
-
 /**
  * void
  */
-
 // 表示没有任何类型
-function warnUser(): void {
-  console.log('This is my warning message')
+function warnUser() {
+    console.log('This is my warning message');
 }
-
 // 声明一个 void 类型的变量没有什么大用，因为你只能为它赋予 undefined 和 null：
-let unusable: void = undefined;
-
+var unusable = undefined;
 /**
  * null 和 undefined
  */
-
-let u: undefined = undefined;
-let n: null = null;
-
+var u = undefined;
+var n = null;
 // 指定了 --strictNullChecks标记，null和undefined只能赋给它们各自
-let num: number = 3;
+var num = 3;
 // num = null; // 报错  Type 'null' is not assignable to type 'number'.
-
 // 使用联合类型可以解决这个问题
-let num1: number | null = 4;
+var num1 = 4;
 num1 = null;
-
 /**
  * never
  */
-
 // 返回never的函数必须存在无法达到的终点
-function error(message: string): never {
-  throw new Error(message)
+function error(message) {
+    throw new Error(message);
 }
-
 // 推断的返回值类型为never
 function fail() {
-  return error("Something failed")
+    return error("Something failed");
 }
-
 // 返回never的函数必须存在无法达到的终点
-function infiniteLoop(): never {
-  while (true) {
-  }
+function infiniteLoop() {
+    while (true) {
+    }
 }
-
-
-/**
- * object
- */
-declare function create(o: object | null): void
-
-create({prop: 0}); // OK
+create({ prop: 0 }); // OK
 create(null); // OK
-
 // create(42); // Error
 // create('string'); // Error
 // create(false); // Error
 // create(undefined); // Error
-
 /**
  * 断言类型
  */
-
 // “尖括号”语法
-let someValue: any = 'this is a string';
-let strLength: number = (<string>someValue).length;
-
+var someValue = 'this is a string';
+var strLength = someValue.length;
 // as语法
-let someValue1: any = 'this is a string';
-let strLength2: number = (someValue as string).length;
+var someValue1 = 'this is a string';
+var strLength2 = someValue.length;
